16ee4113c6fde8e2575829b50dfb725a58336498
Andrei Robson da Silva <andreirobson@furb.br>
2022-07-14 17:10:56 -0300

    Create project

---
+++ backend/src/main/java/com/validadebavihaus/ControleValidadeBavihaus/ControleValidadeBavihausApplication.java
@@ -0,0 +1,13 @@
+package com.validadebavihaus.ControleValidadeBavihaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleValidadeBavihausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleValidadeBavihausApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/validadebavihaus/ControleValidadeBavihaus/ControleValidadeBavihausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validadebavihaus.ControleValidadeBavihaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleValidadeBavihausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
